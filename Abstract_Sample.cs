690996ec0e53b5a9a444e4bc2aa194835390439d
crpraveen <crpraveen@DESKTOP-AS44J1P>
2022-07-19 10:59:41 +0530

    Add project files.

---
+++ Abstract_Sample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concepts
+
+{    // Implementing abstract method
+    abstract class Vehicle //abstract  class 
+    {
+      
+        public abstract void display();
+
+    }
+
+    internal class Bike : Vehicle // abstaract class inherits Bike class 
+    {
+        String ? name;
+
+        public override void display()
+        {
+            Console.WriteLine("Enter Your Vehicle Name :");
+            name=Console.ReadLine();
+            Console.WriteLine("name of  Vehicle is :" + name);
+        }
+    
+        static void Main(string[] args)
+        {
+            Bike owner= new Bike(); // create object
+            owner.display();
+            
+
+        }
+
+    }
+}
+    
+
---
+++ Interface_Sample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concepts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Oops_Concepts
+    {
+        internal interface Animal
+        {
+            void cat();
+            void bird();
+            void fish();
+        }
+        class Nature : Animal  // here implenets the interface
+
+        {
+            //here i implemnts methods from the interface abstract method into derived class non abstract methodd
+            public void cat()
+            {
+                Console.WriteLine("Cat can Walk");
+            }
+            public void bird()
+            {
+                Console.WriteLine("Bird can Fly");
+
+            }
+            public void fish()
+            {
+                Console.WriteLine("Fish can Swim");
+
+            }
+
+            public static void main(String[] args)
+            {
+                Nature n = new Nature();
+                n.fish();
+                n.bird();
+                n.cat();
+            }
+        }
+    }
+
+}
